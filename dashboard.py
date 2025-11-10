4d40dd35849b654fee1fb1bcabf6d5b6c7b405c3
Andres Cedillo <acedillo@centro.edu.mx>
2025-11-10 10:58:59 -0600

    first commit

--- dashboard.py
+++ dashboard.py
@@ -14,8 +14,9 @@ threshold = st.slider("Confidence threshold", 0.0, 1.0,1.5)
 run_button = st.button("Run inference")
 
 
-df = pd.DataFrame(np.random.randn(20,3), columns=["a", "b", "c"])
+df = pd.DataFrame(np.random.randn(  ), columns=["a", "b", "c"])
 st.dataframe(df)
 st.line_chart(df)
+##
 
 
